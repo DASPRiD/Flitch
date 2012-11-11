b63742cefac24d752fcfb88b9c7107b02dda225c
Ben Scholzen <mail@dasprids.de>
2012-11-11 17:36:24 +0100

    Add PHP tags rule

--- src/Flitch/Rule/File/PhpTags.php
+++ src/Flitch/Rule/File/PhpTags.php
@@ -18,6 +18,39 @@ use Flitch\Rule\AbstractRule;
 class PhpTags extends AbstractRule
 {
     /**
+     * Map of tags to their names.
+     *
+     * @var array
+     */
+    protected static $tagMap = array(
+        '<?php' => 'long',
+        '<?'    => 'short',
+        '<?='   => 'short-echo',
+    );
+
+    /**
+     * Allowed PHP tags.
+     *
+     * @var array
+     */
+    protected $allowed = array('long', 'short', 'short-echo');
+
+    /**
+     * Set allowed PHP tags.
+     *
+     * @param  string|array $allowed
+     * @return PhpTags
+     */
+    public function setAllowed($allowed)
+    {
+        if (!is_array($allowed)) {
+            $allowed = array_map('trim', explode(',', $allowed));
+        }
+
+        $this->allowed = array_intersect($allowed, array('long', 'short', 'short-echo'));
+    }
+
+    /**
      * check(): defined by Rule interface.
      *
      * @see    Rule::check()
@@ -26,5 +59,22 @@ class PhpTags extends AbstractRule
      */
     public function check(File $file)
     {
+        $file->rewind();
+
+        while ($file->seekTokenType(T_OPEN_TAG)) {
+            $token = $file->current();
+
+            $tag  = trim($token->getLexeme());
+            $name = self::$tagMap[$tag];
+
+            if (!in_array($name, $this->allowed)) {
+                $this->addViolation(
+                    $file, $token->getLine(), $token->getColumn(),
+                    sprintf('PHP tag "%s" is not allowed', $tag)
+                );
+            }
+
+            $file->next();
+        }
     }
 }
